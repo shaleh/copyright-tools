ac8e3a0a04886aec70e7d4280e44f03903baaab2
Sean Perry <shaleh@speakeasy.net>
2016-01-13 13:13:36 -0800

    Allow arguments to be passed to nosetests.

--- run-tests.sh
+++ run-tests.sh
@@ -6,4 +6,4 @@ export PYTHONPATH=`pwd`/tools
 
 TEST_ENV=${TEST_ENV:-test-env}
 
-${TEST_ENV}/bin/nosetests --with-coverage --cover-package=tools
+${TEST_ENV}/bin/nosetests --with-coverage --cover-package=tools $@
